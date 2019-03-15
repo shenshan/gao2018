7d536404ea7ed54cbb6f80742a9d648911dcf7c0
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-03-15 14:25:39 -0700

    Add Computed table UnitValid.

--- pipeline/ephys.py
+++ pipeline/ephys.py
@@ -2,7 +2,7 @@
 Schema of session information.
 '''
 import datajoint as dj
-from pipeline import reference, acquisition
+from . import reference, acquisition, behavior
 import scipy.io as sio
 import numpy as np
 import os
@@ -95,3 +95,76 @@ class UnitSpikeTimes(dj.Imported):
                 key['unit_cell_type'] = value.cellType
 
             self.insert1(key)
+
+
+@schema
+class ValidUnit(dj.Computed):
+    definition = """
+    -> UnitSpikeTimes
+    ---
+    is_valid:           bool      # check whether a unit is valid based on some criteria
+    number_hit_r:       int       # number of hit r trials for this unit
+    number_err_r:       int       # number of err r trials for this unit
+    number_hit_l:       int       # number of hit l trials for this unit
+    number_err_l:       int       # number of err l trials for this unit
+    """
+
+    def make(self, key):
+        spike_trials = (UnitSpikeTimes & key).fetch1('spike_trials')
+        min_trial = min(spike_trials)
+        max_trial = max(spike_trials)
+        hit_r_trials = behavior.TrialSet.Trial & key & \
+            'trial_response = "HitR"' & \
+            'trial_id between {} and {}'.format(min_trial, max_trial)
+        err_r_trials = behavior.TrialSet.Trial & key & \
+            'trial_response = "ErrR"' & \
+            'trial_id between {} and {}'.format(min_trial, max_trial)
+        hit_l_trials = behavior.TrialSet.Trial & key & \
+            'trial_response = "HitL"' & \
+            'trial_id between {} and {}'.format(min_trial, max_trial)
+        err_l_trials = behavior.TrialSet.Trial & key & \
+            'trial_response = "ErrL"' & \
+            'trial_id between {} and {}'.format(min_trial, max_trial)
+
+        is_valid = len(hit_r_trials) > 15 and len(hit_l_trials) > 15
+        key.update({
+            'number_hit_r': len(hit_r_trials),
+            'number_err_r': len(err_r_trials),
+            'number_hit_l': len(hit_l_trials),
+            'number_err_l': len(err_l_trials),
+            'is_valid': is_valid
+        })
+        self.insert1(key)
+
+
+@schema
+class AlignedPsth(dj.Computed):
+    definition = """
+    -> UnitSpikeTimes
+    ---
+    time_window:            blob        # time window for spike times and psth, 0 is cue start time.
+    spike_times_hit_r:      longblob    # list of spike times on each trial for hit r trials
+    spike_times_err_r:      longblob    # list of spike times on each trial for err r trials
+    spike_times_hit_l:      longblob    # list of spike times on each trial for hit l trials
+    spike_times_err_l:      longblob    # mean firing rate of each stage of err l trials
+    spike_counts_hit_r:     longblob    # mean firing rate of each trial for hit r trials
+    spike_counts_err_r:     longblob    # mean firing rate of each trial for hit r trials
+    spike_counts_hit_l:     longblob    # mean firing rate of each trial for hit r trials
+    spike_counts_err_l:     longblob    # mean firing rate of each trial for hit r trials
+    bins:                   longblob    # time bins for psth computation
+    psth_hit_r:             longblob    # psth for hit r trials
+    psth_err_r:             longblob    # psth for hit r trials
+    psth_hit_l:             longblob    # psth for hit r trials
+    psth_err_l:             longblob    # psth for hit r trials
+    sample_selectivity:     tinyint     # whether selectivity is significant during the sample period
+    delay_selectivity:      tinyint     # whether selectivity is significnat during the delay period
+    response_selectivity:   tinyint     # whether selectivity is significant during the response period
+    trial_ids_screened_hit_r: blob      # trial ids that were screened to calculate the preference, for hit r trials
+    trial_ids_screened_hit_l: blob      # trial ids that were screened to calculate the preference, for hit l trials
+    psth_prefer:            longblob    # psth on preferred hit trials
+    psth_non_prefer:        longblob    # psth on non-preferred hit trials
+    """
+
+    def make(self, key):
+        aligned_psth = key.copy()
+        time_window = [-3.5, 2]
