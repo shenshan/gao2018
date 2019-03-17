e1841ae92335ecbd70b844a074d3a5d57b80a5a0
Shan Shen <shanshen@Shans-MacBook-Pro.local>
2019-03-17 16:14:52 -0700

    Computed table for aligned trials.

---
+++ pipeline/__init__.py
@@ -0,0 +1,85 @@
+
+import numpy as np
+import scipy.signal as signal
+
+
+# helper functions
+def get_trials(key, max_trial, min_trial, trial_type):
+
+    return behavior.TrialSet.Trial & key & \
+        'trial_response = "{}"'.format(trial_type) & \
+        'trial_id between {} and {}'.format(min_trial, max_trial)
+
+
+def get_spk_times(key, spk_times, spk_trials, trials):
+
+    return [spk_times[spk_trials == trial] -
+            (behavior.TrialSet.Trial & key &
+                'trial_id = {}'.format(trial)).proj(
+                    cue_time='trial_cue_time + trial_start_time').fetch1(
+                        'cue_time')
+            for trial in trials]
+
+
+def get_spk_counts(key, spk_times, trials):
+
+    spk_counts = []
+    for itrial, trial in enumerate(trials):
+        pole_in_time, pole_out_time = \
+            (behavior.TrialSet.Trial & key &
+                'trial_id = {}'.format(trial)).proj(
+                    pole_in_time='trial_pole_in_time - trial_cue_time',
+                    pole_out_time='trial_pole_out_time - trial_cue_time'
+            ).fetch1('pole_in_time', 'pole_out_time')
+
+        after_cue_time = 1.3
+        before_pole_in_time = -0.5
+        spk_time = spk_times[itrial]
+
+        spk_counts.append([])
+
+        spk_counts[itrial].append([])
+        # spk counts (spks/sec) in the sampling period
+        spk_counts[itrial][0] = \
+            sum((spk_time < pole_out_time) & (spk_time > pole_in_time)) / \
+            (pole_out_time - pole_in_time)
+
+        spk_counts[itrial].append([])
+        # spk counts (spks/sec) in the delay period
+        spk_counts[itrial][1] = \
+            sum((spk_time < 0) & (spk_time > pole_out_time)) / \
+            (0 - pole_out_time)
+
+        spk_counts[itrial].append([])
+        # spk counts (spks/sec) in the response period (0 to 1.3s after cue)
+        spk_counts[itrial][2] = \
+            sum((spk_time < after_cue_time) & (spk_time > 0)) / after_cue_time
+
+        # spk counts (spks/sec) in the entire period (from pole in to 1.3s
+        # after cue)
+        spk_counts[itrial].append([])
+        spk_counts[itrial][3] = \
+            sum((spk_time < after_cue_time) & (spk_time > pole_in_time)) / \
+            (after_cue_time - pole_in_time)
+
+        # spk counts (spks/sec) in the pre-pole in period (-0.5s to pole_in
+        # time)
+        spk_counts[itrial].append([])
+        spk_counts[itrial][4] = \
+            sum((spk_time < pole_in_time) &
+                (spk_time > pole_in_time - before_pole_in_time)) / \
+            before_pole_in_time
+
+    return spk_counts
+
+
+def get_psth(spk_times, time_bins):
+
+    mean_counts = np.divide(
+        np.histogram(np.hstack(spk_times),
+                     range=[min(time_bins), max(time_bins)],
+                     bins=len(time_bins))[0],
+        len(spk_times))
+
+    # convolve with a box-car filter
+    return signal.convolve(mean_counts, signal.boxcar(200), mode='same')
--- pipeline/ephys.py
+++ pipeline/ephys.py
@@ -3,8 +3,11 @@ Schema of session information.
 '''
 import datajoint as dj
 from . import reference, acquisition, behavior
+from . import get_trials, get_spk_times, get_spk_counts, get_psth
 import scipy.io as sio
+import scipy.stats as ss
 import numpy as np
+from numpy import random
 import os
 import glob
 import re
@@ -107,27 +110,27 @@ class ValidUnit(dj.Computed):
     number_err_r:       int       # number of err r trials for this unit
     number_hit_l:       int       # number of hit l trials for this unit
     number_err_l:       int       # number of err l trials for this unit
+    hit_r_ids=null:     blob      # trial_ids of hit r trials
+    err_r_ids=null:     blob      # trial_ids of err r trials
+    hit_l_ids=null:     blob      # trial_ids of hit l trials
+    err_l_ids=null:     blob      # trial_ids of err l trials
     """
 
     def make(self, key):
         spike_trials = (UnitSpikeTimes & key).fetch1('spike_trials')
         min_trial = min(spike_trials)
         max_trial = max(spike_trials)
-        hit_r_trials = behavior.TrialSet.Trial & key & \
-            'trial_response = "HitR"' & \
-            'trial_id between {} and {}'.format(min_trial, max_trial)
-        err_r_trials = behavior.TrialSet.Trial & key & \
-            'trial_response = "ErrR"' & \
-            'trial_id between {} and {}'.format(min_trial, max_trial)
-        hit_l_trials = behavior.TrialSet.Trial & key & \
-            'trial_response = "HitL"' & \
-            'trial_id between {} and {}'.format(min_trial, max_trial)
-        err_l_trials = behavior.TrialSet.Trial & key & \
-            'trial_response = "ErrL"' & \
-            'trial_id between {} and {}'.format(min_trial, max_trial)
+        hit_r_trials = get_trials(key, min_trial, max_trial, 'HitR')
+        err_r_trials = get_trials(key, min_trial, max_trial, 'ErrR')
+        hit_l_trials = get_trials(key, min_trial, max_trial, 'HitL')
+        err_l_trials = get_trials(key, min_trial, max_trial, 'ErrL')
 
         is_valid = len(hit_r_trials) > 15 and len(hit_l_trials) > 15
         key.update({
+            'hit_r_ids': hit_r_trials.fetch('trial_id'),
+            'err_r_ids': err_r_trials.fetch('trial_id'),
+            'hit_l_ids': hit_l_trials.fetch('trial_id'),
+            'err_l_ids': err_l_trials.fetch('trial_id'),
             'number_hit_r': len(hit_r_trials),
             'number_err_r': len(err_r_trials),
             'number_hit_l': len(hit_l_trials),
@@ -140,31 +143,131 @@ class ValidUnit(dj.Computed):
 @schema
 class AlignedPsth(dj.Computed):
     definition = """
-    -> UnitSpikeTimes
+    -> ValidUnit
     ---
+    mean_fr_hit_r:          longblob    # mean firing rate of each trial for hit r trials
+    mean_fr_err_r=null:     longblob    # mean firing rate of each trial for err r trials
+    mean_fr_hit_l:          longblob    # mean firing rate of each trial for hit l trials
+    mean_fr_err_l=null:     longblob    # mean firing rate of each trial for err l trials
     time_window:            blob        # time window for spike times and psth, 0 is cue start time.
-    spike_times_hit_r:      longblob    # list of spike times on each trial for hit r trials
-    spike_times_err_r:      longblob    # list of spike times on each trial for err r trials
-    spike_times_hit_l:      longblob    # list of spike times on each trial for hit l trials
-    spike_times_err_l:      longblob    # mean firing rate of each stage of err l trials
-    spike_counts_hit_r:     longblob    # mean firing rate of each trial for hit r trials
-    spike_counts_err_r:     longblob    # mean firing rate of each trial for hit r trials
-    spike_counts_hit_l:     longblob    # mean firing rate of each trial for hit r trials
-    spike_counts_err_l:     longblob    # mean firing rate of each trial for hit r trials
     bins:                   longblob    # time bins for psth computation
     psth_hit_r:             longblob    # psth for hit r trials
-    psth_err_r:             longblob    # psth for hit r trials
-    psth_hit_l:             longblob    # psth for hit r trials
-    psth_err_l:             longblob    # psth for hit r trials
-    sample_selectivity:     tinyint     # whether selectivity is significant during the sample period
-    delay_selectivity:      tinyint     # whether selectivity is significnat during the delay period
-    response_selectivity:   tinyint     # whether selectivity is significant during the response period
+    psth_err_r=null:        longblob    # psth for err r trials
+    psth_hit_l:             longblob    # psth for hit l trials
+    psth_err_l=null:        longblob    # psth for err l trials
+    sample_selectivity:     boolean     # whether selectivity is significant during the sample period
+    delay_selectivity:      boolean     # whether selectivity is significnat during the delay period
+    response_selectivity:   boolean     # whether selectivity is significant during the response period
     trial_ids_screened_hit_r: blob      # trial ids that were screened to calculate the preference, for hit r trials
     trial_ids_screened_hit_l: blob      # trial ids that were screened to calculate the preference, for hit l trials
     psth_prefer:            longblob    # psth on preferred hit trials
     psth_non_prefer:        longblob    # psth on non-preferred hit trials
     """
 
+    key_source = ValidUnit & 'is_valid = 1'
+
     def make(self, key):
+        print(key)
         aligned_psth = key.copy()
+        hit_r_ids, err_r_ids, hit_l_ids, err_l_ids = (ValidUnit & key).fetch1(
+            'hit_r_ids', 'err_r_ids', 'hit_l_ids', 'err_l_ids'
+        )
+
+        spk_times, spk_trials = (UnitSpikeTimes & key).fetch1(
+            'spike_times', 'spike_trials')
+
+        photo_stim_type = '0'
+        # spike times
+        spk_times_hit_r = get_spk_times(key, spk_times, spk_trials, hit_r_ids,
+                                        photo_stim_type)
+        spk_times_err_r = get_spk_times(key, spk_times, spk_trials, err_r_ids)
+        spk_times_hit_l = get_spk_times(key, spk_times, spk_trials, hit_l_ids)
+        spk_times_err_l = get_spk_times(key, spk_times, spk_trials, err_l_ids)
+
+        # spike counts in different stages
+        spk_counts_hit_r = get_spk_counts(key, spk_times_hit_r, hit_r_ids)
+        spk_counts_err_r = get_spk_counts(key, spk_times_err_r, err_r_ids)
+        spk_counts_hit_l = get_spk_counts(key, spk_times_hit_l, hit_l_ids)
+        spk_counts_err_l = get_spk_counts(key, spk_times_err_l, err_l_ids)
+
+        # compute convoluted psth
         time_window = [-3.5, 2]
+        bins = np.arange(time_window[0], time_window[1]+0.001, 0.001)
+
+        psth_hit_r = get_psth(spk_times_hit_r, bins)
+        if np.size(spk_times_err_r):
+            psth_err_r = get_psth(spk_times_err_r, bins)
+            aligned_psth.update({
+                'mean_fr_err_r': np.mean(spk_counts_err_r, axis=0),
+                'psth_err_r': psth_err_r
+            })
+        psth_hit_l = get_psth(spk_times_hit_l, bins)
+        if np.size(spk_times_err_l):
+            psth_err_l = get_psth(spk_times_err_l, bins)
+            aligned_psth.update({
+                'mean_fr_err_l': np.mean(spk_counts_err_l, axis=0),
+                'psth_err_l': psth_err_l
+            })
+
+        # check selectivity
+        result_sample = ss.ttest_ind(spk_counts_hit_r[:][0],
+                                     spk_counts_hit_l[:][0])
+        result_delay = ss.ttest_ind(spk_counts_hit_r[:][1],
+                                    spk_counts_hit_l[:][1])
+        result_response = ss.ttest_ind(spk_counts_hit_r[:][2],
+                                       spk_counts_hit_l[:][2])
+
+        # prefered psth
+        random.shuffle(hit_r_ids)
+        random.shuffle(hit_l_ids)
+
+        spk_times_hit_r_screen = get_spk_times(key, spk_times, spk_trials,
+                                               hit_r_ids[:10])
+        mean_fr_hit_r_screen = np.mean(get_spk_counts(key,
+                                                      spk_times_hit_r_screen,
+                                                      hit_r_ids[:10]),
+                                       axis=0)
+
+        spk_times_l_screen = get_spk_times(key, spk_times, spk_trials,
+                                           hit_l_ids[:10])
+        mean_fr_hit_l_screen = np.mean(get_spk_counts(key,
+                                                      spk_times_hit_r_screen,
+                                                      hit_l_ids[:10]),
+                                       axis=0)
+
+        spk_times_hit_r_test = get_spk_times(key, spk_times, spk_trials,
+                                             hit_r_ids[10:])
+        spk_counts_hit_r_test = get_spk_counts(key, spk_times_hit_r_test,
+                                               hit_r_ids[10:])
+
+        spk_times_hit_l_test = get_spk_times(key, spk_times, spk_trials,
+                                             hit_l_ids[10:])
+        spk_counts_hit_l_test = get_spk_counts(key, spk_times_hit_l_test,
+                                               hit_l_ids[10:])
+
+        psth_hit_r_test = get_psth(spk_times_hit_r_test, bins)
+        psth_hit_l_test = get_psth(spk_times_hit_l_test, bins)
+        if mean_fr_hit_r_screen[3] > mean_fr_hit_r_screen[3]:
+            psth_prefer = psth_hit_r_test
+            psth_non_prefer = psth_hit_l_test
+        else:
+            psth_prefer = psth_hit_l_test
+            psth_non_prefer = psth_hit_r_test
+
+        aligned_psth.update({
+            'mean_fr_hit_r': np.mean(spk_counts_hit_r, axis=0),
+            'mean_fr_hit_l': np.mean(spk_counts_hit_l, axis=0),
+            'time_window': time_window,
+            'bins': bins,
+            'psth_hit_r': psth_hit_r,
+            'psth_hit_l': psth_hit_l,
+            'sample_selectivity': bool(result_sample.pvalue < 0.05),
+            'delay_selectivity':  bool(result_delay.pvalue < 0.05),
+            'response_selectivity': bool(result_delay.pvalue < 0.05),
+            'trial_ids_screened_hit_r': hit_r_ids[:10],
+            'trial_ids_screened_hit_l': hit_l_ids[:10],
+            'psth_prefer': psth_prefer,
+            'psth_non_prefer': psth_non_prefer
+        })
+
+        self.insert1(aligned_psth)
