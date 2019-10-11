f7c6d4560ca153d47667d11bb708b2e00c37ea86
shenshan <shenshanpku@gmail.com>
2019-10-10 23:34:21 -0700

    Rename processing script.

--- gao2018.sh
+++ gao2018.sh
@@ -1,2 +1,2 @@
 docker-compose up -d
-docker exec gao2018_datajoint_1 /bin/bash -c "bash /src/gao2018/scripts/gao2018.sh"
+docker exec gao2018_datajoint_1 /bin/bash -c "bash /src/gao2018/scripts/gao2018_processing.sh"
