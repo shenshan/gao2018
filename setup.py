2e46684c4f1cc196cdcace18472bd7e6f59d15f5
shenshan <shenshanpku@gmail.com>
2020-03-24 23:21:43 +0000

    Add pynwb into requirements

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='gao_2018_pipeline',
-    version='0.0.0',
+    version='0.0.1',
     description='Datajoint schemas for Gao et al., 2018 from Li Lab',
     author='Vathes',
     author_email='support@vathes.com',
     packages=find_packages(exclude=[]),
-    install_requires=['datajoint'],
+    install_requires=['datajoint>=0.12', 'pynwb'],
     scripts=['scripts/gao2018-shell.py'],
 )
